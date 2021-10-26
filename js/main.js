340af319c4a54cb9416356e5da74f59c3800c5fe
manumartinz <manumartinzx@gmail.com>
2021-10-26 12:28:29 -0300

    Updated

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/* const zapatilla = {
+    marca: prompt('Ingrese la marca de la zapatilla.'),
+    color: prompt('Ingrese el color que busca.'),
+    precio: prompt('Ingrese el precio de la zapatilla.'),
+
+    calcIva: function() {
+        let iva = this.precio * 1.21;
+        return iva;
+    },
+
+    mostrarZapatilla: function() {
+        let mostrar = `Marca: ${this.marca}, Color: ${this.color} y `;
+        return mostrar;
+    }
+};
+
+alert(`${zapatilla.mostrarZapatilla()}el precio final es de $${zapatilla.calcIva()} (iva incluido).`); */
